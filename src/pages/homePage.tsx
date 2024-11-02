1bf8ef1df80ce6c0f6e3895b3cbbe79e68fa92af
MatinGhasemi <[c.matinghasemi@gmail.com]>
2024-11-02 01:16:26 -0700

    Setup Tailwind

---
+++ src/pages/homePage.tsx
@@ -0,0 +1,14 @@
+import Navbar from "../components/navbar";
+
+const HomePage = () => {
+    return ( 
+        <>  
+            <Navbar />
+            <div className="text-4xl">
+                Home
+            </div>
+        </>
+     );
+}
+ 
+export default HomePage;
